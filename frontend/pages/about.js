22565794bccdab1831aaad5ee4135614e61dd6d5
Srajan Bansal <srajanbansal999@gmail.com>
2022-08-03 21:04:01 +0530

    index.js has been updated

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const about = () => {
   return (
-    <div  className="container  mx-auto px-4">
+    <div  className="container mx-auto px-4">
       About
     </div>
   )
