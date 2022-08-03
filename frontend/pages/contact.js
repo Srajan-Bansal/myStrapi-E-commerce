0e0f09d4ef726a392c2633105150653b80c02414
Srajan Bansal <srajanbansal999@gmail.com>
2022-08-03 20:59:33 +0530

    Product page added

---
+++ frontend/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div  className="container mx-auto px-4">
+      Products
+    </div>
+  )
+}
+
+export default contact
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,11 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
-import Navbar from '../component/Navbar'
 
 export default function Home() {
   return (
-    <div>
-      <Navbar/>
+    <div className="container mx-auto px-4">
+      This is Homepage
     </div>
   )
 }
