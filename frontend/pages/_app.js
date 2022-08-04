fd7d13d8d56514a1b1c5b3710e219f69f3514e24
Srajan Bansal <srajanbansal999@gmail.com>
2022-08-05 03:39:01 +0530

    Added a feature in Buy Now button in product

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,41 @@
 import '../styles/globals.css'
 import NavBar from '../component/NavBar'
+import { useEffect, useState } from 'react'
+
+
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    return () => {
+  
+    }
+  }, [])
+
+  const [cart, setCart] = useState([]);
+  const [reloadKey, setReloadKey] = useState(1);
+
+  const addToCart = (item, qty, price) => {
+    let newCart = cart;
+    for (let index = 0; index < qty; index++) {
+      cart.push([item, price]);
+    }
+    setCart(newCart)
+    setReloadKey(Math.random());
+  }
+  const removeToCart = (item, qty) => {
+    let newCart = cart;
+    let index = newCart.indexOf(item);
+    newCart.splice(index);
+    setCart(newCart)
+  }
+  const clearCart = (item) => {
+    setCart([]);
+  }
+
   return <>
-    <NavBar />
-    <Component {...pageProps} />
+    <NavBar key={reloadKey} cart={cart} />
+    <Component cart={cart} removeToCart={removeToCart} addToCart={addToCart} clearCart={clearCart} {...pageProps} />
   </>
 }
 
